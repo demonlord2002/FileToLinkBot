61fa91adf0fbab33a7c04c765a2fb912d9e0416f
demonlord2002 <drdoom2003p@gmail.com>
2025-11-06 23:16:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 BIN_CHANNEL = int(os.getenv("BIN_CHANNEL", "0"))
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 
-FQDN = os.getenv("FQDN", "https://your-app-name.herokuapp.com")
+# FQDN will be added after first deployment
+FQDN = os.getenv("FQDN", "")
 HAS_SSL = os.getenv("HAS_SSL", "True").lower() == "true"
-
 PORT = int(os.getenv("PORT", "8080"))
 NO_PORT = os.getenv("NO_PORT", "True").lower() == "true"
